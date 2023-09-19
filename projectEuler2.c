ea73942830b2a36463b4b3fbcb322a502668db94
MatheusVidigal <120541900+MatheusVidigalUFES@users.noreply.github.com>
2023-09-19 08:25:29 -0300

    Add files via upload

---
+++ projectEuler2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main() {
+   int s1=1,s2=2,s3,num, sum=0, n;
+   printf("Introduza um inteiro: ");
+   scanf("%d",&n);
+   for (num=1;num<=n;num=s1)
+   {
+      s3=s2;
+      s2+=s1;
+      s1=s3;
+      if ( num%2 == 0)
+         sum += num;
+   }
+   printf("A soma é igual a: %d\n",sum);
+   
+   
+}
