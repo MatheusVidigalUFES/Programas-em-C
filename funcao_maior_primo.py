c9b63f04e188ee1391f13f0962d0b1088417004b
MatheusVidigal <120541900+MatheusVidigalUFES@users.noreply.github.com>
2023-09-23 22:12:25 -0300

    Project Euler Problem 3
    O programa responde bem para números menores que 20000. A partir deste valor, é iniciado o gargalo. Continuarei a verificar o que pode ser feito para tornar o programa viável para números relativamente grandes.

---
+++ funcao_maior_primo.py
@@ -0,0 +1,22 @@
+def ehPrimo_positivo(n):
+    if n<=1:
+        return False
+    for i in range(2,n//2+1):
+        if n==2 or n==3:
+            break
+        elif n%i!=0:
+            continue
+        else:
+            return False
+    return True     
+
+def maior_fator_num(num):
+    if ehPrimo_positivo(num) ==True:
+        return num
+    lista_divisor_num = [] # lista dos primos que dividem num
+    for i in range(num//2+1):
+        if ehPrimo_positivo(i)==True:
+            if num%i==0:
+                lista_divisor_num.append(i)
+    return max(lista_divisor_num)
+
